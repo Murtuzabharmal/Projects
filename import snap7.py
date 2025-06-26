410cb1de0a44d7f1159478b05f20665b12776687
Murtuzabharmal <117078768+Murtuzabharmal@users.noreply.github.com>
2025-06-26 21:15:31 +0200

    Update import snap7.py

--- import snap7.py
+++ import snap7.py
@@ -1,17 +1,15 @@
 import snap7
 from snap7.server import Server
+from snap7 import types
 import time
 
-# Create a Snap7 server (S7-300/400)
 server = Server()
-
-# Memory size: simulate 1KB data blocks
 data = (bytearray(1024), bytearray(1024), bytearray(1024))
-server.register_area(snap7.types.areas['DB'], 1, data[0])
-server.register_area(snap7.types.areas['DB'], 2, data[1])
-server.register_area(snap7.types.areas['DB'], 3, data[2])
 
-# Start server on default IP
+server.register_area(types.areas['DB'], 1, data[0])
+server.register_area(types.areas['DB'], 2, data[1])
+server.register_area(types.areas['DB'], 3, data[2])
+
 server.start()
 print("S7 Server is running...")
 
